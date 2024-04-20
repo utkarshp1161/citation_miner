0fe47221e99787c6a8c714c2fea171cac5ac777c
utkarshp1161 <utkarshp1161@gmail.com>
2024-04-20 14:35:08 -0400

    ignore paper with no year info in metadata

--- citation_miner/script.py
+++ citation_miner/script.py
@@ -23,13 +23,13 @@ def get_last_n_year_info(author_a, current_year, n=5):
         pub_year = int(publication['bib']['pub_year']) 
       except:
         print(" the publication metadata doesnt have year info lets anyway add this in the list and substitue it with current year -- this is a hack")
-        pub_year = current_year + n + 5
+        pub_year = current_year - n - 5
           
       if current_year - pub_year <= n:# this filters based on year
           jj+= 1
           print(f"accepted publication from year{pub_year}")
           print(f"{jj}th paper accepeted till now")
-          if jj % 50 == 0:
+          if jj % 10 == 0:
               print("we have queried 50 papers in a row lets wait 30 seconds for avoid getting banned")
               time.sleep(30)  # Rate limiting
 
